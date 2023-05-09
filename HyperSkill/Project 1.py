d546308261a7465e780acb3443ca2dc39bc4c912
lyfewyse <docarmowm@gmail.com>
2023-05-09 11:28:57 -0400

    First Hyperskill Project
    First project. Hyperskill counted it as correct, but it doesn't quite seem to be when run in the terminal. Will need to revisit this.

---
+++ HyperSkill/Project 1.py
@@ -0,0 +1,18 @@
+print("Earned amount: ")
+print("Bubblegum: $202")
+print("Toffee: $118")
+print("Ice cream: $2250")
+print("Milk chocolate: $1680")
+print("Doughnut: $1075")
+print("Pancake: $80")
+print()
+print("Income: $5405")
+income = 5405
+print("Staff expenses: ")
+staff_expenses = int(input())
+print("Other expenses: ")
+other_expenses = int(input())
+net_income = (income - (staff_expenses + other_expenses))
+
+print("Net Income: ")
+print(net_income)
