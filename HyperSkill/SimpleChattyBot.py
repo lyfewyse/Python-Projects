df8becf6f21ad017e27e3da3dd1e76d47c475da9
lyfewyse <docarmowm@gmail.com>
2023-05-11 11:52:21 -0400

    Updated to "function" format

--- HyperSkill/SimpleChattyBot.py
+++ HyperSkill/SimpleChattyBot.py
@@ -1,28 +1,62 @@
-print('Hello! My name is Alva.')
-print('I was created in 2023.')
-print('Please, remind me your name.')
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
 
-# reading a name
-user_name = input()
-print(f'What a great name you have, {user_name}!')
-print('Let me guess your age.')
-print('Enter remainders of dividing your age by 3, 5 and 7.')
 
-# reading all remainders
-remainder3 = int(input("Remainder 3: "))
-remainder5 = int(input("Remainder 5: "))
-remainder7 = int(input("Remainder 7: "))
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
 
-your_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
 
-print(f"Your age is {your_age}; that's a good time to start programming!")
-print('Now I will prove to you that I can count to any number you want.')
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
 
-# read a number and count to it here
-print('Enter a number:')
-number = 0
-user = int(input())
-while number <= user:
-    print(number, "!")
-    number += 1
-print('Completed, have a nice day!')
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    print("Your age is " + str(age) +
+          "; that's a good time to start programming!")
+
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    user = int(input())
+    number = 0
+    while number <= user:
+        print(number, '!')
+        number += 1
+
+
+def test():
+    print("Let's test your programming knowledge.")
+    questions = ["Why do we use methods?", "1. To repeat a statement multiple times.", "2. To decompose a program into several small subroutines.",
+                 "3. To determine the execution time of a program.", "4. To interrupt the execution of a program."]
+    # prints all [questions] instead of manually writing each print funciton
+    for n in questions:
+        print(n)
+    # user input
+    answer = int(input())
+    # loop
+    while answer != 2:
+        print("Please, try again.")
+        answer = int(input())
+    if answer == 2:
+        print('Completed, have a nice day!')
+
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+# function calls
+greet('Alva', '2023')  # change it as you need
+remind_name()
+guess_age()
+count()
+test()
+end()
