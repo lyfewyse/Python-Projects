ce6b251027cafc9e4aad250198fbe2d71aeadd61
lyfewyse <118470212+lyfewyse@users.noreply.github.com>
2023-06-08 15:59:11 -0400

    File path update

--- HyperSkill/Hospital Data Analysis/Analysis.py
+++ HyperSkill/Hospital Data Analysis/Analysis.py
@@ -4,9 +4,9 @@ import pandas as pd
 df = pd.set_option('display.max_columns', 8)
 
 # Read CSV files
-general = pd.read_csv("test/general.csv")
-prenatal = pd.read_csv("test/prenatal.csv")
-sports = pd.read_csv("test/sports.csv")
+general = pd.read_csv("HyperSkill/Hospital Data Analysis/test/general.csv")
+prenatal = pd.read_csv("HyperSkill/Hospital Data Analysis/test/prenatal.csv")
+sports = pd.read_csv("HyperSkill/Hospital Data Analysis/test/sports.csv")
 
 # rename columns
 # Change column names in prenatal DataFrame
